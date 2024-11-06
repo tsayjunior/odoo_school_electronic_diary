189d914fc0e20f4f6984ce016e80e0a88a0288b6
Junior Javier Llanos Duran <juniorllanos15@gmail.com>
2024-11-06 18:31:42 -0400

    changes for herency in odoo

--- addons/agenda_escolar/models/models.py
+++ addons/agenda_escolar/models/models.py
@@ -28,4 +28,6 @@ class subject(models.Model):
     _name = 'agenda_escolar.subject'
     _description = 'agenda_escolar.subject'
     
-    name = fields.Char()
+    name = fields.Char(string="Nombre", readonly=False, required=True, help='Introduzca el Nombre')
+    description = fields.Text(string="Descripcion", readonly=False, required=False, help='Introduzca la descripcion')
+    code = fields.Char(string="Codigo", readonly=False, required=True, help='Introduzca el codigo')
